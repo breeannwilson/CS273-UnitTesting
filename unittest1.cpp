42b1e64efcfce2aeb863ded56676998348ff466c
breeannwilson <breeannmwilson@gmail.com>
2017-06-06 11:06:25 -0700

    Add files via upload
    files for testing read_int

--- unittest1.cpp
+++ unittest1.cpp
@@ -1,64 +1,94 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "Triangle.h"
-#include "Square.h"
+#include <fstream>
+#include <iostream>
+#include "readint.h"
+#include <stdexcept>
 
+using namespace std;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTester
 {		
-	TEST_CLASS(TriangleTest)		//This is our test suite for testing isTriangle()
+	TEST_CLASS(READINT_TEST)
 	{
 	public:
-
-		TEST_METHOD(AllDifferent)   //Test Case 1: all the sides are different
-		{
-			//We are checking that isTriangle() should return true
-			Assert::AreEqual(isTriangle(3, 4, 5), true);
-		}
-
-		TEST_METHOD(TooLong)		//Test Case 2: one side is too long
-		{
-			//We are checking that isTriangle() should return false
-			Assert::AreEqual(isTriangle(3, 3, 7), false);
-		}
-
-		TEST_METHOD(Zeros)			//Test Case 3: all sides zero
-		{
-			//We are checking that isTriangle() should return false
-			Assert::AreEqual(isTriangle(0, 0, 0), false);
-		}
-
-		TEST_METHOD(Negatives)		//Test Case 4: all sides negative
-		{
-			//We are checking that isTriangle() should return false
-			Assert::AreEqual(isTriangle(-1, -1, -1), false);
-		}
-	};
-
-
-	TEST_CLASS(SquaresTest) {		//This is the test class for isSquare()
-	public:
 		
-		TEST_METHOD(AllSame)		//Test Case 1: all sides are the same length (& positive)
+		TEST_METHOD(TestCase1)		//when input is not int
 		{
-			//We are checking that isSquare() should return true
-			Assert::AreEqual(isSquare(4, 4, 4, 4), true);
+			//open a file stream to read the file zeroinput.txt
+			ifstream ss("C:\\Users\\breea\\OneDrive\\documents\\visual studio 2017\\Projects\\CS273 Exception Handling\\UnitTester\\zeroinput.txt");
+
+			//check that the file stream was opened successfully
+			if (ss.fail())
+				throw int(-1);
+
+			//replace the cin read buffer with the read buffer from the file stream
+			streambuf *orig_cin = cin.rdbuf(ss.rdbuf());
+
+			//perform the read_int() test
+			//first will read "Hello", not valid but read_int will go through
+			//the while loop and will have "0" inputed next, then will return
+			//0
+			Assert::AreEqual(read_int("My prompt: ", -3, 3), 0);
+
+			//restore cin to the way it was before
+			cin.rdbuf(orig_cin);
+
+			//close the file stream
+			ss.close();
+
 		}
 
-		TEST_METHOD(Zeros)			//Test Case 2: all sides are zeros
+
+		TEST_METHOD(TestCase2)		//for invalid argument
 		{
-			//We are checking that isSquare() should return false
-			Assert::AreEqual(isSquare(0, 0, 0, 0), false);
+			//define func
+			auto func = []() {
+				//call with incorrect arguments to test for invalid_argument
+				read_int("My prompt: ", 5, 1);
+			};
+
+			//We expect an invalid_argument exception to be thrown when we call func
+			Assert::ExpectException<std::invalid_argument>(func);
 		}
 
-		TEST_METHOD(Negatives)		//Test Case 3: all sides are negative
+
+		TEST_METHOD(TestCase3)		//to make sure read_int works when input is valid
 		{
-			//We are checking that isSquare() should return false
-			Assert::AreEqual(isSquare(-1, -1, -1, -1), false);
+			//open a file stream to read the file testcase3.txt
+			ifstream ss("C:\\Users\\breea\\OneDrive\\documents\\visual studio 2017\\Projects\\CS273 Exception Handling\\UnitTester\\testcase3.txt");
+
+			//check that the file stream was opened successfully
+			if (ss.fail())
+				throw int(-1);
+
+			//replace the cin read buffer with the read buffer from the file stream
+			streambuf *orig_cin = cin.rdbuf(ss.rdbuf());
+
+			//perform the read_int() test
+			//test will pass if "3" is returned
+			Assert::AreEqual(read_int("My prompt: ", 0, 4), 3);
+
+			//restore cin to the way it was before
+			cin.rdbuf(orig_cin);
+
+			//close the file stream
+			ss.close();
 		}
 
 
+		TEST_METHOD(TestCase4)		//for invalid argument
+		{
+			//define func
+			auto func = []() {
+				//call with incorrect arguments to test for invalid_argument
+				read_int("My prompt: ", 0, 0);
+			};
+
+			//We expect an invalid_argument exception to be thrown when we call func
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
 	};
 }
